69cb7c39ec384889a57e361cef260535b5f37dee
thierry.richol <thierry.richol@gmail.com>
2024-10-17 03:56:21 +0000

    fix: anomalie affichage image

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -13,78 +13,89 @@ class WelcomeScreen extends StatelessWidget {
         height: double.infinity,
         width: double.infinity,
         decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color(0xffB81736),
-                  Color(0xff281537),
-                ]
-            )
-        ),
-        child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 200.0),
-                child: Image(image: AssetImage('assets/logo.png')),
+            gradient: LinearGradient(colors: [
+          Color(0xffB81736),
+          Color(0xff281537),
+        ])),
+        child: Column(children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Image(image: AssetImage('assets/logo.png')),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          const Text(
+            'Welcome Back',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const loginScreen()));
+            },
+            child: Container(
+              height: 53,
+              width: 320,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.white),
               ),
-              const SizedBox(
-                height: 100,
-              ),
-              const Text('Welcome Back',style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white
-              ),),
-              const SizedBox(height: 30,),
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const loginScreen()));
-                },
-                child: Container(
-                  height: 53,
-                  width: 320,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Colors.white),
-                  ),
-                  child: const Center(child: Text('SIGN IN',style: TextStyle(
+              child: const Center(
+                child: Text(
+                  'SIGN IN',
+                  style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white
-                  ),),),
+                      color: Colors.white),
                 ),
               ),
-              const SizedBox(height: 30,),
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const RegScreen()));
-                },
-                child: Container(
-                  height: 53,
-                  width: 320,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(color: Colors.white),
-                  ),
-                  child: const Center(child: Text('SIGN UP',style: TextStyle(
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const RegScreen()));
+            },
+            child: Container(
+              height: 53,
+              width: 320,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.white),
+              ),
+              child: const Center(
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.black
-                  ),),),
+                      color: Colors.black),
                 ),
               ),
-              const Spacer(),
-              const Text('Login with Social Media',style: TextStyle(
-                  fontSize: 17,
-                  color: Colors.white
-              ),),//
-              const SizedBox(height: 12,),
-              const Image(image: AssetImage('assets/social.png'))
-            ]
-        ),
+            ),
+          ),
+          const Spacer(),
+          const Text(
+            'Login with Social Media',
+            style: TextStyle(fontSize: 17, color: Colors.white),
+          ), //
+          const SizedBox(
+            height: 12,
+          ),
+          const Image(image: AssetImage('assets/logo_google.png')),
+          const SizedBox(
+            height: 12,
+          ),
+        ]),
       ),
-
     );
   }
 }
